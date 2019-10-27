a4ac3a8a9b35f43550bef2403579413f3f274a5d
Chris Johns <9357528+ChrisJohns-me@users.noreply.github.com>
2019-10-27 11:26:49 -0600

    Update setup-template.h

--- src/setup-template.h
+++ src/setup-template.h
@@ -13,10 +13,15 @@ char *MQTT_STATE_TOPIC_PREFIX = "light/"; // e.g. light/led and light/led/set
 #define OTApassword "PASSWORD" //the password you will need to enter to upload remotely via the ArduinoIDE
 #define OTAport 8266
 
-#define LED_PIN 14 // D5 on ESP8266
-#define LED_COUNT 12
-#define NEO_PIXEL_TYPE NEO_RGB + NEO_KHZ800
+#define LED1_PIN 5 // D1
+#define LED1_COUNT 61
+#define LED2_PIN 4 // D2
+#define LED2_COUNT 50
+#define LED3_PIN 14 // D5
+#define LED3_COUNT 400
+
+#define NEO_PIXEL_TYPE NEO_BRG + NEO_KHZ800
 #define DEFAULT_EFFECT FX_MODE_RAINBOW_CYCLE
-#define DEFAULT_POWER_ON_STATE true
+#define DEFAULT_POWER_ON_STATE false
 
 char *deviceName = "led";
